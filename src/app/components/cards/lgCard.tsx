cbadab46cbcf42ca13d99271ea75bae6310026ce
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-05-11 18:10:22 -0300

    center section & lg card

---
+++ src/app/components/cards/lgCard.tsx
@@ -0,0 +1,30 @@
+type LgCardTypes = {
+    title:string,
+    description:string,
+    colors:{
+        secondary:string,
+        primary:string,
+        text:string
+    },
+    icon:any
+}
+
+export default function LgCard({title, description, colors, icon}: LgCardTypes) {
+    return (
+      <div className={`relative border-2 border-black ${colors.primary} px-8 sm:px-5 md:px-8 pb-6 pt-14 sm:pt-5 rounded-2xl`}>
+        <div className={`font-bold mb-7 text-2xl ${colors.text} sm:max-w-[75%]`} >{title}</div>
+        <p className={`text-base sm:text-lg ${colors.text}`}>{description}</p>
+        <div className="absolute -top-9 sm:top-2 md:top-3 right-3 sm:right-1 md:right-3" >
+            <div className="relative">
+                <div className={`absolute -top-1 right-1 ${colors.secondary} p-2 rounded-2xl border-2 border-black`} >
+                    {icon}
+                </div>
+                <div className={`bg-black p-2 rounded-2xl border-2 border-black`} >
+                    {icon}
+                </div>
+            </div>
+        </div>
+      </div>
+    );
+  }
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,20 @@ import GridSection from "./components/gridSection";
 import Footer from "./components/footer";
 import Form from "./components/form";
 import PaymentOptions from "./components/paymentOptions";
+import CenterSection from "./components/centerSection";
+import TwoColSection from "./components/twoColSection";
 
 export default function Home() {
   return (
     <main className="">
-      <Header/>
-      <Hero/>
-      <GridSection/>
-      <PaymentOptions/>
-      <Form/>
-      <Footer/>
+      <Header />
+      <Hero />
+      <GridSection />
+      <TwoColSection/>
+      <CenterSection />     
+      <PaymentOptions />
+      <Form />
+      <Footer />
     </main>
-  )
+  );
 }
