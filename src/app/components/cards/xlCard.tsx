9cbf4111c70fbde6b5c25190fd8880a353dc0e9d
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-05-12 13:02:34 -0300

    xl cards finished

--- src/app/components/cards/xlCard.tsx
+++ src/app/components/cards/xlCard.tsx
@@ -1,26 +1,36 @@
 import Image from "next/image";
-import think from "public/think.svg";
 
-export default function XlCard() {
+type XlCardType = {
+  content:{
+    title:string,
+    description:string
+  },
+  colors:{
+    background:string,
+    text:string
+  }
+  image:any,
+  reverse:boolean
+}
+
+
+export default function XlCard({content, colors, image, reverse}:XlCardType) {
   return (
-    <div className="relative w-full md:grid md:grid-cols-12 items-center">
-      <div className="z-10 relative flex flex-col items-center md:border-2 md:border-black md:bg-blue-500 md:rounded-2xl md:grid md:col-start-2 col-end-13 md:grid-cols-11 py-6 md:py-16">
-        <div className="md:absolute mb-5 md:mb-0 md:-top-[20%] md:-left-[20%] m-auto z-10 w-full max-w-[90%] sm:max-w-[80%] md:max-w-none md:w-auto md:col-end-6">
-          <Image src={think} alt="think" className="block w-full" />
+    <div className="relative w-full md:grid md:grid-cols-12 items-center mb-24">
+      <div className={`z-10 relative flex flex-col items-center md:border-2 md:border-black md:${colors.background} md:rounded-2xl md:grid md:grid-cols-11 ${reverse ? 'md:col-start-1 col-end-12':'md:col-start-2 col-end-13'} py-6 md:py-16`}>
+        <div className={`md:absolute mb-5 md:mb-0 m-auto z-10 w-full max-w-[90%] sm:max-w-[80%] md:max-w-none md:w-auto md:-top-[20%] ${reverse ? 'md:-right-[20%] md:col-start-7 md:col-end-13':'md:-left-[20%] md:col-start-1 md:col-end-6'}`}>
+          <Image src={image} alt="think" className="block w-full" />
         </div>
-        <div className="max-w-[477px] z-10 text-white md:col-start-6 md:col-end-12 mx-4 md:ml-3 md:mr-16">
+        <div className={`max-w-[477px] z-10 ${colors.text} mx-4 ${reverse?'md:col-start-1 md:col-end-7 md:mr-3 md:ml-16':'md:col-start-6 md:col-end-12 md:ml-3 md:mr-16'}`}>
           <div className="font-bold mb-4 text-2xl">
-            Efficient Appointment Management
+            {content.title}
           </div>
           <p className="text-lg">
-            With DentalTrack, managing appointments has never been easier. Our
-            app allows dentists to schedule and reschedule appointments, mark
-            them as completed, and track cancellations and no-shows, all in one
-            place. Say goodbye to confusing schedules and missed appointments!
+            {content.description}
           </p>
         </div>
       </div>
-      <div className="z-0 absolute top-[20%] sm:top-[25%] border-2 border-black bg-blue-500 rounded-2xl w-full h-[80%] md:hidden"></div>
+      <div className={`z-0 absolute top-[23%] sm:top-[25%] border-2 border-black ${colors.background} rounded-2xl w-full h-[80%] md:hidden`}></div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import PaymentOptions from "./components/paymentOptions";
 import CenterSection from "./components/centerSection";
 import TwoColSection from "./components/twoColSection";
 import XlCard from "./components/cards/xlCard";
+import think from "public/think.svg";
+import offices from "public/offices.svg";
 
 export default function Home() {
   return (
@@ -15,10 +17,29 @@ export default function Home() {
       <Hero />
       <GridSection />
       <div className="max-w-5xl m-auto mb-24 px-2 lg:px-0">
-        <XlCard/>
+        <XlCard
+          content={{
+            title: "Efficient Appointment Management",
+            description:
+              "With DentalTrack, managing appointments has never been easier. Our app allows dentists to schedule and reschedule appointments, mark them as completed, and track cancellations and no-shows, all in one place. Say goodbye to confusing schedules and missed appointments!",
+          }}
+          colors={{ background: "bg-blue-500", text: "text-white" }}
+          image={think}
+          reverse={false}
+        />
+        <XlCard
+          content={{
+            title: "Customizable Office Setup",
+            description:
+              "DentalTrack is designed to adapt to the unique needs of each dental practice. Dentists can easily create custom offices and organize appointments based on location. This makes it easy to keep track of appoint-ments and ensure that everyone is on the same page.",
+          }}
+          colors={{ background: "bg-neutral-100", text: "text-balck" }}
+          image={offices}
+          reverse={true}
+        />
       </div>
-      <TwoColSection/>
-      <CenterSection />     
+      <TwoColSection />
+      <CenterSection />
       <PaymentOptions />
       <Form />
       <Footer />
