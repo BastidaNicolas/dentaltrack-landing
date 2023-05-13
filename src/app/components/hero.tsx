7a8fa55328e3d1d1b2c3a0ea9ece27a867eab956
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-05-13 12:45:42 -0300

    lower image quality

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -20,7 +20,7 @@ export default function Hero() {
           </div>
           <NotifyBtn text={"Get Notified"} />
         </div>
-        <Image src={MobileApp} alt="mobile app view" className="min-w-[80%] sm:min-w-[328px]" priority/>
+        <Image src={MobileApp} alt="mobile app view" className="min-w-[80%] sm:min-w-[328px]" priority quality={80} />
       </div>
     </div>
   );
