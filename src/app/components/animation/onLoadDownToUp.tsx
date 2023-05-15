42ebc3cd1625160d7784fdd720d13c19d3a7d239
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-05-14 22:32:34 -0300

    duration set to 0.3

--- src/app/components/animation/onLoadDownToUp.tsx
+++ src/app/components/animation/onLoadDownToUp.tsx
@@ -6,7 +6,7 @@ export default function OnLoadDownToUp({ children }: any) {
     <motion.div
         initial={{ opacity: 0, y:50 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.2 }}
+        transition={{ duration: 0.3 }}
     >
       {children}
     </motion.div>
--- src/app/components/animation/onLoadFadeIn.tsx
+++ src/app/components/animation/onLoadFadeIn.tsx
@@ -6,7 +6,7 @@ export default function OnLoadFadeIn({ children }: any) {
     <motion.div
         initial={{ opacity: 0}}
         animate={{ opacity: 1}}
-        transition={{ duration: 0.2 }}
+        transition={{ duration: 0.3 }}
     >
       {children}
     </motion.div>
