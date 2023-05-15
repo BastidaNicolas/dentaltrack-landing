bd2040360d774d73c1ee7137a7e661a789bca5b6
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-05-14 22:25:02 -0300

    made onload anim take less

--- src/app/components/animation/onLoadDownToUp.tsx
+++ src/app/components/animation/onLoadDownToUp.tsx
@@ -6,7 +6,7 @@ export default function OnLoadDownToUp({ children }: any) {
     <motion.div
         initial={{ opacity: 0, y:50 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.3 }}
+        transition={{ duration: 0.1 }}
     >
       {children}
     </motion.div>
--- src/app/components/animation/onLoadFadeIn.tsx
+++ src/app/components/animation/onLoadFadeIn.tsx
@@ -6,7 +6,7 @@ export default function OnLoadFadeIn({ children }: any) {
     <motion.div
         initial={{ opacity: 0}}
         animate={{ opacity: 1}}
-        transition={{ duration: 0.3 }}
+        transition={{ duration: 0.1 }}
     >
       {children}
     </motion.div>
