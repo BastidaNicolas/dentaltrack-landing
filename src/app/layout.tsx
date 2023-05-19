42334200f23d99db66009825c8ce55848bd9c7d5
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-05-19 17:11:34 -0300

    optimized meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,10 @@ import { Roboto } from "next/font/google";
 const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 export const metadata = {
+  
   title: "DentalTrack - Simplify Your Dental Appointment Scheduling",
   description:
-    "DentalTrack simplifies dental appointments scheduling and patient reminders, making it easy for dentists to manage their practice and provide top-notch patient care. Our cloud-based software enables dentists to create customizable appointment schedules, automate appointment reminders and securely store patient records. Sign up today and take your dental practice to the next level with DentalTrack.",
+    "DentalTrack simplifies dental appointments scheduling & reminders, empowering dentists to manage their practice with ease. Sign up now for customizable schedules, automated reminders & secure patient records. Elevate your dental practice with DentalTrack.",
 };
 
 export default function RootLayout({
@@ -17,6 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="">
       <head>
+        <meta property="og:image" content="https://i.imgur.com/ELRu623.jpg" />
+        <meta property="og:image:type" content="image/jpg" />
+        <meta property="og:title" content="DentalTrack - Simplify Your Dental Appointment Scheduling" />
+        <meta property="og:description" content="DentalTrack simplifies dental appointments scheduling & reminders, empowering dentists to manage their practice with ease. Sign up now for customizable schedules, automated reminders & secure patient records. Elevate your dental practice with DentalTrack." />
         <meta property="og:url" content="https://www.dentaltrack.ar/" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image"></meta>
@@ -40,8 +45,6 @@ export default function RootLayout({
           name="twitter:text:title"
           content="DentalTrack simplifies dental appointments scheduling and patient reminders, making it easy for dentists to manage their practice and provide top-notch patient care. Our cloud-based software enables dentists to create customizable appointment schedules, automate appointment reminders and securely store patient records. Sign up today and take your dental practice to the next level with DentalTrack."
         />
-        <meta property="og:image" content="https://i.imgur.com/ytnzvzD.jpg" />
-        <meta property="og:image:type" content="image/jpg" />
       </head>
       <body className={`${roboto.className}`}>{children}</body>
     </html>
