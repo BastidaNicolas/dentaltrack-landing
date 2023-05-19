5fc7a3b2a83ea48b133cd3676e7bc892facc4552
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-05-19 20:33:18 -0300

    removed text and padding

--- src/app/components/notifyBtn.tsx
+++ src/app/components/notifyBtn.tsx
@@ -8,15 +8,14 @@ type notifyBtnType = {
 export default function NotifyBtn({ text }: notifyBtnType) {
   return (
     <Link
-      className="relative py-2 px-5 font-bold text-lg sm:text-base capitalize group block w-fit"
+      className="relative font-bold text-lg sm:text-base capitalize group block w-fit"
       href={"/#form"}
       scroll={false}
     >
-      {text}
       <div className="z-10 absolute top-0 left-0 group-active:translate-x-0 group-active:translate-y-0 -translate-x-1 -translate-y-1 duration-100 bg-white font-bold text-lg sm:text-base capitalize border-2 border-black rounded-lg py-2 px-5 truncate ">
         {text}
       </div>
-      <div className="z-0 absolute top-0 left-0 bg-black font-bold text-lg sm:text-base capitalize border-2 border-black rounded-lg py-2 px-5 truncate ">
+      <div className="z-0 bg-black font-bold text-lg sm:text-base capitalize border-2 border-black rounded-lg py-2 px-5 truncate ">
         {text}
       </div>
     </Link>
