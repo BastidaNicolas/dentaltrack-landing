83c782369a4e55c52f1c50f2925856ff22ec1625
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-05-21 18:21:07 -0300

    image meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="">
       <head>
-        <meta property="og:image" content="https://i.imgur.com/ELRu623.jpg" />
+        <meta name="image" property="og:image" content="https://i.imgur.com/ELRu623.jpg"></meta>
         <meta property="og:image:type" content="image/jpg" />
         <meta
           property="og:title"
