57fa93a07707eb836e2db1447029c51d99969ff2
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-05-22 15:49:47 -0300

    keywords, icon added to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,26 @@ export const metadata = {
   title: "DentalTrack - Simplify Your Dental Appointment Scheduling",
   description:
     "DentalTrack simplifies dental appointments scheduling & reminders, empowering dentists to manage their practice with ease. Sign up now for customizable schedules, automated reminders & secure patient records. Elevate your dental practice with DentalTrack.",
+  keywords: [
+    "DentalTrack",
+    "Appointment scheduling",
+    "Appointment management software",
+    "Dentist",
+    "Dental clinic",
+    "Management software",
+    "Schedule",
+    "Appointment",
+    "Notification",
+    "Dental practice",
+    "Patient management",
+    "Automated reminders",
+    "Dental office",
+    "Streamline workflow",
+    "appointment system",
+  ],
+  icons: {
+    icon: '/Group-91.png',
+  }
 };
 
 export default function RootLayout({
