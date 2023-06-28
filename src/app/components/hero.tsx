01431721180ae5697e9f73e8c74e0aaba9f0f7ab
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-06-27 21:20:23 -0300

    + mock text messages

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -8,26 +8,42 @@ export default function Hero() {
     <>
       <Header />
       <div className="border-b-2 border-b-black bg-blue-600 pt-6 md:pt-16 pb-8 md:pb-16 px-3 lg:px-0">
-          <div className="relative w-full max-w-7xl m-auto flex flex-wrap md:px-3 xl:px-6 2xl:px-0 md:flex-nowrap justify-end md:justify-between">
-            <div className="mb-10 sm:mb-16 w-full z-10 ">
-              <div className="leading-none max-w-2xl text-[clamp(48px,_13vw,_96px)] md:text-[clamp(72px,_7.8vw,_96px)] font-extrabold drop-shadow-[0_4px_0_#141414] sm:drop-shadow-[0_4px_0_#141414] tracking-[-0.016em] text-white mb-8 text-outline">
-                Simplify patient appointment management.
-              </div>
-              <p className="text-white md:font-bold mb-4 text-xl md:max-w-xl">
-                DentalTrack helps dental practices efficiently manage their
-                appointments and send reminders to patients, reducing missed
-                appointments and increasing productivity.
-              </p>
-              {/* <div className="md:font-bold text-white mb-1 text-xl md:max-w-xl">
+        <div className="relative w-full max-w-7xl m-auto flex flex-wrap md:px-3 xl:px-6 2xl:px-0 md:flex-nowrap justify-end md:justify-between">
+          <div className="mb-10 sm:mb-16 w-full z-20 ">
+            <div className="leading-none max-w-2xl text-[clamp(48px,_13vw,_96px)] md:text-[clamp(72px,_7.8vw,_96px)] font-extrabold drop-shadow-[0_4px_0_#141414] sm:drop-shadow-[0_4px_0_#141414] tracking-[-0.016em] text-white mb-8 text-outline">
+              Simplify patient appointment management.
+            </div>
+            <p className="text-white md:font-bold mb-4 text-xl md:max-w-xl">
+              DentalTrack helps dental practices efficiently manage their
+              appointments and send reminders to patients, reducing missed
+              appointments and increasing productivity.
+            </p>
+            {/* <div className="md:font-bold text-white mb-1 text-xl md:max-w-xl">
                 Want to be notified when DentalTrack is available?
               </div> */}
-              <NotifyBtn text={"Get Notified"} style="" />
+            <NotifyBtn text={"Get Notified"} style="" />
+          </div>
+          <div className="relative md:absolute md:right-0 md:top-0 h-[clamp(400px,_100vw,_576px)] md:h-full w-[500px] md:w-[clamp(72px,_28vw,_328px)]">
+            <div className="" >
+              <div className="absolute z-10 bg-green-400 text-base border-black border-2 rounded-lg max-w-[300px] py-2 px-4 md:-left-60 bottom-80">
+                Hey, I want a thing to get my tooth checked bro!
+              </div>
+              <div className="absolute z-10 bg-neutral-100 border-black border-2 rounded-lg max-w-[300px] py-2 px-4 left-8 md:-left-40 bottom-60">
+                K bro. I've got for tomorrow at 13 or at 18
+              </div>
+              <div className="absolute z-10 bg-green-400 border-black border-2 rounded-lg max-w-[300px] py-2 px-4  md:-left-60 bottom-44">
+                18 sounds good!
+              </div>
+              <div className="absolute z-10 bg-neutral-100 border-black border-2 rounded-lg max-w-[300px] py-2 px-4 left-8 md:-left-40 bottom-16">
+                K bro. I've got for tomorrow at 13 and at 18. Does one of these
+                fit your schedule?
+              </div>
             </div>
-            <div className="relative md:absolute md:right-0 md:top-0 h-[clamp(400px,_100vw,_576px)] md:h-full w-full md:w-[clamp(72px,_28vw,_328px)] md:mx-3 xl:mx-6 2xl:mx-0">
+            <div className="h-[clamp(400px,_100vw,_576px)] md:h-full w-[clamp(72px,_50vw,_328px)] md:w-[clamp(72px,_28vw,_328px)]">
               <Image
                 src={MobileApp}
                 alt="mobile app view"
-                className="object-contain"
+                className="object-contain z-0"
                 fill
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 30vw"
                 priority={true}
@@ -35,6 +51,7 @@ export default function Hero() {
               />
             </div>
           </div>
+        </div>
       </div>
     </>
   );
