e016eea13284f5e544df7ac46d7057ebbaaeba4c
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-06-29 19:35:09 -0300

    changed sizes

--- src/app/components/cards/lgCard.tsx
+++ src/app/components/cards/lgCard.tsx
@@ -17,14 +17,14 @@ export default function LgCard({
 }: LgCardTypes) {
   return (
     <div className="w-full border-4 border-black rounded-2xl bg-blue-600">
-      <div className="h-80 md:h-[336px] bg-slate-100 rounded-t-xl ">
+      <div className="h-60 sm:h-[336px] bg-slate-100 rounded-t-xl ">
         {/*add animated svg*/}
       </div>
-      <div className="h-max p-7 sm:p-8 border-t-4 border-black rounded-b-xl text-white">
-        <div className="font-bold text-3xl sm:text-4xl mb-3 sm:mb-4 capitalize">
+      <div className="h-max p-4 sm:p-8 border-t-4 border-black rounded-b-xl text-white">
+        <div className="font-bold text-xl sm:text-4xl mb-3 sm:mb-4 capitalize">
           {title}
         </div>
-        <p className="text-lg sm:text-2xl">{description}</p>
+        <p className="text-base sm:text-2xl">{description}</p>
       </div>
     </div>
   );
