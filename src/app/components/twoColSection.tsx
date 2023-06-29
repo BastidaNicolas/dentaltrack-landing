d24a35a28a827415da5eb9bd9e536925f541abf6
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-06-29 19:50:43 -0300

    - centerSection

--- src/app/components/twoColSection.tsx
+++ src/app/components/twoColSection.tsx
@@ -4,7 +4,7 @@ import LgCard from "./cards/lgCard";
 export default function TwoColSection() {
   return (
     <>
-      <div className="grid grid-cols-1 px-3 xl:px-6 2xl:px-0 md:grid-cols-2 gap-8 md:gap-8 lg:gap-16 max-w-7xl m-auto mb-24">
+      <div className="grid grid-cols-1 px-3 xl:px-6 2xl:px-0 md:grid-cols-2 gap-8 md:gap-8 lg:gap-16 max-w-7xl m-auto">
         <LgCard
           title="Powerfull Tagging System"
           description="Identify and manage appointments based on the services provided. Dentists can quickly see what kind of appointment is scheduled and what they need to do to prepare."
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,6 @@ export default function Home() {
           />
         </div>
         <TwoColSection />
-        <CenterSection />
         <PaymentOptions />
         <QueryClientComponent>
           <Form />
