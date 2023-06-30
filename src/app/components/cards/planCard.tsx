6dbe1ad86da2b8934aa482bc2e78f4996a0e0a2d
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-06-30 19:45:41 -0300

    redesigned

--- src/app/components/cards/planCard.tsx
+++ src/app/components/cards/planCard.tsx
@@ -1,4 +1,4 @@
-import NotifyBtn from "../notifyBtn";
+import Link from "next/link";
 
 type PlanCardType = {
   content: {
@@ -12,13 +12,17 @@ type PlanCardType = {
 
 function DisabledPlanCard({ content }: PlanCardType) {
   return (
-    <div className="bg-white border-2 border-neutral-500 text-neutral-500 py-6 px-5 rounded-2xl min-h-[395px] h-full w-full">
-      <div className="font-bold text-3xl capitalize mb-4">{content.title}</div>
-      <div className="flex items-end mb-4">
-        <div className="text-4xl mr-2">${content.price}</div>
-        <div className="capitalize text-xl">{`per ` + content.type}</div>
+    <div className="drop-shadow-[4px_4px_0px_rgb(115_115_115)] duration-100 min-h-[423px] bg-white border-4 border-neutral-500 text-neutral-500 p-6 xl:p-8 rounded-2xl h-full w-full">
+      <div className="font-bold text-4xl capitalize mb-4 truncate">
+        {content.title}
       </div>
-      <ul className="list-disc ml-5 text-base mb-8 capitalize">
+      <div className="flex items-end mb-4">
+        <div className="text-5xl mr-2">${content.price}</div>
+        <div className="capitalize text-xl truncate">
+          {`per ` + content.type}
+        </div>
+      </div>
+      <ul className="list-disc ml-5 text-base xl:text-lg mb-8 capitalize">
         {content.features.map((item, index) => (
           <li key={index}>{item}</li>
         ))}
@@ -29,42 +33,27 @@ function DisabledPlanCard({ content }: PlanCardType) {
 
 export default function PlanCard({ content }: PlanCardType) {
   return (
-    <div className="relative">
+    <div className="max-w-[354px] w-full">
       {!content.disabled ? (
-        <>
-          <div className="absolute top-0 left-0 hover:-translate-x-1 hover:-translate-y-1 duration-75 min-h-[395px] bg-white border-2 border-black py-6 px-5 rounded-2xl h-full w-full">
-            <div className="font-bold text-3xl capitalize mb-4">
+        <Link href={"#form"}>
+          <div className="drop-shadow-[4px_4px_0px_#000000] active:drop-shadow-[0px_0px_0px_#000000] hover:cursor-pointer duration-100 min-h-[423px] bg-white border-4 border-black p-6 md:p-8 rounded-2xl h-full w-full">
+            <div className="font-bold text-4xl capitalize mb-4 truncate">
               {content.title}
             </div>
             <div className="flex items-end mb-4">
-              <div className="text-4xl mr-2">${content.price}</div>
-              <div className="capitalize text-xl">{`per ` + content.type}</div>
+              <div className="text-5xl mr-2">${content.price}</div>
+              <div className="capitalize text-xl truncate">
+                {`per ` + content.type}
+              </div>
             </div>
-            <ul className="list-disc ml-5 text-base mb-8 capitalize">
+            <ul className="list-disc ml-5 text-base xl:text-lg mb-8 capitalize">
               {content.features.map((item, index) => (
                 <li key={index}>{item}</li>
               ))}
             </ul>
-            <NotifyBtn text="get notified" style="" />
+            {/* <NotifyBtn text="get notified" style="" /> */}
           </div>
-          <div className=" bg-black border-2 border-black py-6 px-5 rounded-2xl w-full min-h-[395px]">
-            <div className="font-bold text-3xl capitalize mb-4">
-              {content.title}
-            </div>
-            <div className="flex items-end mb-4">
-              <div className="text-4xl mr-2">${content.price}</div>
-              <div className="capitalize text-xl">{`per ` + content.type}</div>
-            </div>
-            <ul className="list-disc ml-5 text-base mb-8 capitalize">
-              {content.features.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-            <div className="bg-white font-bold text-base capitalize border-2 border-black rounded-lg py-2 px-3 truncate  ">
-              get notified
-            </div>
-          </div>
-        </>
+        </Link>
       ) : (
         <DisabledPlanCard content={content} />
       )}
--- src/app/components/paymentOptions.tsx
+++ src/app/components/paymentOptions.tsx
@@ -91,68 +91,67 @@ export default function PaymentOptions() {
 
   return (
     <div
-      className="max-w-5xl m-auto flex flex-col sm:items-center items-start px-3 lg:px-0 pt-24 sm:pt-36"
+      className="max-w-7xl m-auto flex flex-col items-center px-3 xl:px-6 2xl:px-0 pt-24 sm:pt-36"
       id="pricing"
     >
-      <OnViewPopIn>
-        <div className="text-3xl font-bold capitalize mb-6">
-          Choose you're subscription plan
-        </div>
-        <div className="mb-7 flex sm:justify-center">
-          <div className="relative mr-4">
-            <button
-              className={`absolute top-0 left-0 ${
-                pantType !== "monthly" && "-translate-x-1 -translate-y-1"
-              } duration-75 border-black border-2 py-2 px-5 font-bold text-black text-lg rounded-lg z-10 bg-white`}
-              onClick={() => setPlanType("monthly")}
-            >
-              Monthy
-            </button>
-            <div className="border-black border-2 py-2 px-5 font-bold text-black text-lg rounded-lg bg-black">
-              Monthy
-            </div>
+      <div className="text-4xl sm:text-5xl text-center font-bold capitalize mb-9">
+        Choose you're subscription plan
+      </div>
+      <div className="mb-7 flex sm:justify-center">
+        <div className="relative mr-4">
+          <button
+            className={`absolute top-0 left-0 ${
+              pantType !== "monthly" && "-translate-x-1 -translate-y-1"
+            } duration-75 border-black border-2 py-2 px-5 font-bold text-black text-lg rounded-lg z-10 bg-white`}
+            onClick={() => setPlanType("monthly")}
+          >
+            Monthy
+          </button>
+          <div className="border-black border-2 py-2 px-5 font-bold text-black text-lg rounded-lg bg-black">
+            Monthy
           </div>
-          <div className="relative">
-            <button
-              className={`absolute top-0 left-0 ${
-                pantType !== "yearly" && "-translate-x-1 -translate-y-1"
-              } duration-75 border-black border-2 py-2 px-6 font-bold text-black text-lg rounded-lg z-10 bg-white`}
-              onClick={() => setPlanType("yearly")}
+        </div>
+        <div className="relative">
+          <button
+            className={`absolute top-0 left-0 ${
+              pantType !== "yearly" && "-translate-x-1 -translate-y-1"
+            } duration-75 border-black border-2 py-2 px-6 font-bold text-black text-lg rounded-lg z-10 bg-white`}
+            onClick={() => setPlanType("yearly")}
+          >
+            Yearly
+          </button>
+          <div className="border-black border-2 py-2 px-6 font-bold text-black text-lg rounded-lg bg-black relative">
+            Yearly
+            <svg
+              className="absolute -right-7 sm:-right-1/4 top-5 sm:-top-5 -rotate-12"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              Yearly
-            </button>
-            <div className="border-black border-2 py-2 px-6 font-bold text-black text-lg rounded-lg bg-black relative">
-              Yearly
-              <svg
-                className="absolute -right-7 sm:-right-1/4 top-5 sm:-top-5 -rotate-12"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M3 10H13C15.1217 10 17.1566 10.8429 18.6569 12.3431C20.1571 13.8434 21 15.8783 21 18V20M3 10L9 16M3 10L9 4"
-                  stroke="black"
-                  strokeWidth="3"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              <div className="text-xl sm:text-2xl font-bold absolute truncate -right-10 sm:-right-28 -bottom-7 sm:bottom-1/4">
-                20% OFF
-              </div>
+              <path
+                d="M3 10H13C15.1217 10 17.1566 10.8429 18.6569 12.3431C20.1571 13.8434 21 15.8783 21 18V20M3 10L9 16M3 10L9 4"
+                stroke="black"
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <div className="text-xl sm:text-2xl font-bold absolute truncate -right-10 sm:-right-28 -bottom-7 sm:bottom-1/4">
+              20% OFF
             </div>
           </div>
         </div>
-      </OnViewPopIn>
+      </div>
 
-      <OnViewDownToUp>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-8 sm:gap-x-5 w-full">
-          {RenderCards(pantType)}
-        </div>
-      </OnViewDownToUp>
-      <div className="pt-5 font-bold capitalize px-2">displayed subscription plan pricing structure is subject to change prior launch date.</div>
+      <div className="grid grid-flow-col gap-4 xl:gap-8 w-full overflow-y-auto p-1">
+        {RenderCards(pantType)}
+      </div>
+      {/* <div className="pt-5 font-bold capitalize px-2 text-center max-w-[370px]">
+        displayed subscription plan pricing structure is subject to change prior
+        launch date.
+      </div> */}
     </div>
   );
 }
